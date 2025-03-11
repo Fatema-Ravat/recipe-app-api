1fabe3494aff7f5a109062c3d227f502f3c41fe5
Fatema-Ravat <fate.lane@gmail.com>
2025-03-11 16:31:25 +0530

    user_model_changes1

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,31 @@
-from django.db import models  # noqa
+""" Core models """
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, #for authentication system
+                                 PermissionsMixin, #for permissions and fields
+                                 BaseUserManager, )
+
+
+class UserManager(BaseUserManager):
+    """ Manager for user object """
+
+    def create_user(self, email, password = None, **extra_fields):
+        """ Create save and return the user"""
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ Custom user class """
+
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
+
